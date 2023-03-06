5888ee8fc48528656759e9b273c5f02d8409c66d
Irina <bcg>
2023-03-06 20:01:22 +0300

    ouch

--- DataGen/DataGen/main.cpp
+++ DataGen/DataGen/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
-int main() {
-	cout << "Hello, colleague";
+void dataGen(fstream* fData)
+{
+    if (fData)
+    {
+        string charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        for (int i = 0; i < 100; i++)
+        {
+            string result;
+            int length = 1 + (rand() % 9);
+            result.resize(length);
+            for (int j = 0; j < length; j++)
+                result[j] = charset[rand() % charset.length()];
+            double rating = 1.0 + (double)(rand() % (9 * 10)) / 10;
+            *fData << "Example_" << result << " " << rating << endl;
+        }
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+    cout << "Enter file name:" << endl;
+    string fName;
+    cin >> fName;
+    fstream fData(fName, ios::out | ios::trunc);
+    if (fData)
+    {
+        dataGen(&fData);
+        fData.close();
+    }
+    return 0;
 }
